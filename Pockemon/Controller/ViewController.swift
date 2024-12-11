44dd63ebec9722647b4d05e094a921714510043f
solnamul <sol7113@gmail.com>
2024-12-12 01:40:21 +0900

    feat : Lv.1 더미 UI 구현

---
+++ Pockemon/Controller/ViewController.swift
@@ -0,0 +1,141 @@
+//
+//  ViewController.swift
+//  Pockemon
+//
+//  Created by t2023-m105 on 11/12/24.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+    
+    // 테이블 뷰 선언
+    let tableView = UITableView()
+    
+    // 더미 데이터
+    let dummyData = [
+        ("피카츄", "010-0000-0000"),
+        ("라이츄", "010-0000-0000"),
+        ("꼬부기", "010-0000-0000"),
+        ("파이리", "010-0000-0000"),
+        ("이상해씨", "010-0000-0000")
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "친구 목록"
+        
+        // "추가" 버튼
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "추가", style: .plain, target: self, action: #selector(addFriend))
+        
+        setupTableView()
+    }
+    
+    // 테이블 뷰 설정
+    func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(FriendCell.self, forCellReuseIdentifier: "FriendCell")
+        tableView.rowHeight = 80 // 셀 높이 설정
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        
+        // 오토레이아웃 설정
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+    
+    // "추가" 버튼 액션
+    @objc func addFriend() {
+        print("친구 추가 버튼 클릭!")
+    }
+}
+
+// MARK: - UITableViewDataSource, UITableViewDelegate
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dummyData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FriendCell", for: indexPath) as? FriendCell else {
+            return UITableViewCell()
+        }
+        let data = dummyData[indexPath.row]
+        cell.configure(name: data.0, phoneNumber: data.1)
+        return cell
+    }
+}
+
+// MARK: - Custom UITableViewCell
+class FriendCell: UITableViewCell {
+    
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 30 // 원형으로 만들기 위해 반지름 설정
+        imageView.layer.borderWidth = 1 // 테두리 두께
+        imageView.layer.borderColor = UIColor.gray.cgColor // 테두리 색상
+        imageView.clipsToBounds = true // 이미지가 원 밖으로 나가지 않도록 설정
+        imageView.backgroundColor = .white // 기본 프로필 배경색
+        return imageView
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let phoneLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.textColor = .gray
+        label.textAlignment = .right // 번호를 오른쪽 정렬
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.addSubview(profileImageView)
+        contentView.addSubview(nameLabel)
+        contentView.addSubview(phoneLabel)
+        
+        // 오토레이아웃 설정
+        NSLayoutConstraint.activate([
+            // 프로필 이미지
+            profileImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            profileImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            profileImageView.widthAnchor.constraint(equalToConstant: 60),
+            profileImageView.heightAnchor.constraint(equalToConstant: 60),
+            
+            // 이름 (가운데 정렬)
+            nameLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            nameLabel.leadingAnchor.constraint(equalTo: profileImageView.trailingAnchor, constant: 16),
+            
+            // 번호 (오른쪽 정렬)
+            phoneLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            phoneLabel.leadingAnchor.constraint(greaterThanOrEqualTo: nameLabel.trailingAnchor, constant: 8),
+            phoneLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(name: String, phoneNumber: String) {
+        nameLabel.text = name
+        phoneLabel.text = phoneNumber
+    }
+}
