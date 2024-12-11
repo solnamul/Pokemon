4e2f9da56d5e6ca9eb7cc4f7ab92cd3c35ee96e2
solnamul <sol7113@gmail.com>
2024-12-12 01:06:23 +0900

    Initial Commit

---
+++ Pockemon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pockemon
+//
+//  Created by t2023-m105 on 12/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
